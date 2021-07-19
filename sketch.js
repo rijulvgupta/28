dae9c04928ee93515eb6223bda710d5b82ef7104
rijulvgupta <80291914+rijulvgupta@users.noreply.github.com>
2021-07-19 17:39:45 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1, mango2, mango3, mango4, mango5, mango6, mango7
+var stone;
+var elastic1
+var world,boy;
+
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(1000,100,30);
+	mango3=new mango(1000,200,30);
+	mango4=new mango(1200,130,30);
+	mango5=new mango(1100,200,30);
+	mango6=new mango(1250,200,30);
+	mango7=new mango(900,200,30);
+	stone1=new stone(200,340,30);
+
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	elastic1 = new elastic(stone1.body,{x:235,y:420});
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+  
+  
+  elastic1.display();
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  mango7.display();
+  stone1.display();
+  detectcollision(stone1,mango1);
+  detectcollision(stone1,mango2);
+  detectcollision(stone1,mango3);
+  detectcollision(stone1,mango4);
+  detectcollision(stone1,mango5);
+  detectcollision(stone1,mango6);
+  detectcollision(stone1,mango7);
+  
+
+  groundObject.display();
+
+  fill(0)
+  textSize(20)
+text("Press space to get another chance to play",50,50,400)
+}
+function mouseDragged(){
+	Matter.Body.setPosition(stone1.body,{x:mouseX,y:mouseY})
+ }
+ 
+ function mouseReleased(){
+	 elastic1.fly()
+ }
+ function detectcollision(stone1,mango){
+	mangoBodyPosition=mango.body.position
+	stoneBodyPosition=stone1.body.position
+
+	var distance=dist(stoneBodyPosition.x, stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=mango.r+stone1.r){
+		Matter.Body.setStatic(mango.body,false)
+	}
+}
+
+function keyPressed(){
+	if(keyCode===32){
+	Matter.Body.setPosition(stone1.body,{x:235, y:420})
+	elastic1.attach(stone1.body)
+	}
+}
+
